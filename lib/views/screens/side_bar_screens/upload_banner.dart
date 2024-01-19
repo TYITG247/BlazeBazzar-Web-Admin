5fa024f6ba352bebc95fe250734481f452e6ea2d
NirajSahu0412 <nirajsahu0412@gmail.com>
2024-01-19 17:15:57 +0530

    Section 13

--- lib/views/screens/side_bar_screens/upload_banner.dart
+++ lib/views/screens/side_bar_screens/upload_banner.dart
@@ -1,21 +1,91 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
-class UploadBannerScreen extends StatelessWidget {
+class UploadBannerScreen extends StatefulWidget {
   static const String routeName = "\UploadBannerScreen";
+
+  @override
+  State<UploadBannerScreen> createState() => _UploadBannerScreenState();
+}
+
+class _UploadBannerScreenState extends State<UploadBannerScreen> {
+
+  dynamic _image;
+  String? fileName;
+  pickImage() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      allowMultiple: false,
+      type: FileType.image,
+    );
+    if (result != null) {
+      setState(() {
+        _image = result.files.first.bytes;
+        fileName = result.files.first.name;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        alignment: Alignment.topLeft,
-        padding: const EdgeInsets.all(10),
-        child: const Text(
-          'Upload Banner Screen',
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 36,
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Upload Banner Screen',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 36,
+              ),
+            ),
           ),
-        ),
+          Divider(
+            color: Colors.grey,
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 150,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        border: Border.all(color: Colors.black45),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: _image != null
+                          ? Image.memory(
+                              _image,
+                              fit: BoxFit.cover,
+                            )
+                          : Center(
+                              child: Text("Upload Banner"),
+                            ),
+                    ),
+                    Gap(20),
+                    ElevatedButton(
+                      onPressed: () {
+                        pickImage();
+                      },
+                      child: Text("Upload Image"),
+                    ),
+                  ],
+                ),
+              ),
+              Gap(15),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Save"),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
